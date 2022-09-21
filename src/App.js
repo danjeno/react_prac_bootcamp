5b54bb7fcc2ff62bdc6bfe2991e9904935815315
Daniil Jenotov <danila1176@gmail.com>
2022-09-21 18:42:03 +0300

    createContext test

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
 import UserCardsPage from "./pages/userCardsPage/UserCardsPage";
+import TestComponent from "./TestComponent";
 
 function App() {
-  return <UserCardsPage />;
+  // return <UserCardsPage />;
+  return <TestComponent />;
 }
 
 export default App;
